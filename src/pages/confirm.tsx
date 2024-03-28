628620ffa5ca0a51c1ce897270223c3f5cb6b2b7
RNikita123t <9752rathorenikita@gmail.com>
2024-03-28 19:05:47 +0530

    modified order-status

--- src/pages/confirm.tsx
+++ src/pages/confirm.tsx
@@ -1,24 +1,3 @@
-// import OrderConfirmed from "../components/Order/OrderConfirmed";
-// import OrderFailed from "../components/Order/OrderFailed";
-// import React from "react";
-
-// const Confirmation: React.FC = () => {
-//   const renderOrderConfirmed = () => {
-//     const randomValue = Math.random();
-//     const probabilityThreshold = 0.8;
-//     return randomValue < probabilityThreshold ? (
-//       <OrderConfirmed />
-//     ) : (
-//       <OrderFailed />
-//     );
-//   };
-
-//   return <div className="p-8">{renderOrderConfirmed()}</div>;
-// };
-
-// export default Confirmation;
-
-
 import OrderConfirmed from "../components/Order/OrderConfirmed";
 import OrderFailed from "../components/Order/OrderFailed";
 import React from "react";
--- src/pages/order-status.tsx
+++ src/pages/order-status.tsx
@@ -1,37 +1,3 @@
-// import CartProduct from "../components/CartDetails/CartProduct";
-// import Confirmation from "./confirm";
-
-// const CheckoutPage = () => {
-//   const isLocalStorageAvailable =
-//     typeof window !== "undefined" && window.localStorage;
-//   const cartItems = isLocalStorageAvailable
-//     ? JSON.parse(localStorage.getItem("cartItems") || "[]")
-//     : [];
-
-//   return (
-//     <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-12 py-6 md:py-8 lg:py-12">
-//       <div className="flex flex-col lg:flex-row gap-8 items-center">
-//         <div className="lg:flex-1">
-//           <Confirmation />
-//         </div>
-//         <div className="lg:flex-1">
-//           <div className="bg-white rounded-lg shadow-lg p-8">
-//             <h2 className="text-lg font-bold mb-3">Order Summary</h2>
-//             <p className="text-gray-400 mb-8">Check your items</p>
-//             <div className="space-y-4">
-//               {cartItems.map((item: any) => (
-//                 <CartProduct key={item.id} {...item} />
-//               ))}
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CheckoutPage;
-
 import CartProduct from "../components/CartDetails/CartProduct";
 import Confirmation from "./confirm";
 import { useState } from "react";
